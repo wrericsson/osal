5cf048725a9eca12154c9da04b9dd2d75267df1d
wrericsson <rui-wang2002@163.com>
2016-12-29 13:55:32 +0800

    remove the error symbol
    Signed-off-by: wrericsson <rui-wang2002@163.com>

--- master/src/memleak/module.mk
+++ master/src/memleak/module.mk
@@ -5,13 +5,11 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS  += -Werror
 LOCAL_CFLAGS  += -LOCAL_CFLAGS
-LOCAL_LDFLAGS += -Wl,--wrap=malloc -Wl,--wrap=free -Wl,--wrap=realloc -Wl,--wrap=calloc
+LOCAL_LDFLAGS += -Wl,--wrap=malloc -Wl,--wrap=free -Wl,--wrap=realloc --Wl,--wrap=open  Wl,--wrap=calloc
 
 LOCAL_C_INCLUDES += $(ROOT_DIR)/include/memleak 
-#LOCAL_SRC_FILES += memleak.c
-#LOCAL_SRC_FILES += me
 LOCAL_SRC_DIR := $(LOCAL_PATH)
-LOCAL_SRC_FILES += $(shell find $(LOCAL_SRC_DIR) -name '*.c' | sed s:^$(LOCAL_PATH)::g )
+LOCAL_SRC_FILES += $(shell find $(LOCAL_SRC_DIR) -name '*.c' | sed s:^$(LOCAL_PATH)/::g )
 LOCAL_MODULE := libmemleak
 
 include $(BUILD_STATIC_LIBRARY)
